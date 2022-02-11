2be4bd633f3dd7f0e10903edbec3fd1814996419
Alex Vyber <alexvyber@gmail.com>
2022-02-11 13:25:17 +0300

    deleted: some shit has been deleted

--- .local/bin/change-kitty-theme.sh
+++
@@ -1,8 +0,0 @@
-##!/usr/bin/env bash
-
-theme_name=$(find ~/.config/kitty/kitty-themes/themes/ -type f | fzf ) 
-
-echo "$theme_name"
-
-rm ~/.config/kitty/theme.conf ;
-ln -s "$theme_name" ~/.config/kitty/theme.conf
--- .local/bin/kitty.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/home/alexs/.local/kitty.app/bin/kitty
