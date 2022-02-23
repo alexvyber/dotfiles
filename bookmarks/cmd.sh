86e7541d453a25fd2cc8be899fce489e3c527ae2
Alex Vyber <alexvyber@gmail.com>
2022-02-23 15:21:03 +0300

    upd(cmd): new command

--- bookmarks/cmd.sh
+++ bookmarks/cmd.sh
@@ -6,3 +6,4 @@ for pid in $(pgrep kitty) ; do kill -9 $pid ; done
 for x in {1..12} ; do echo -n "F$x " ; tput kf$x | cat -A ; echo ; done
 find . -type f | fzf --preview "bat --style=numbers --color=always --line-range :500 {}"
 ga. && g cm "upd(dot-files): regular stuff in dot-files"
+tree . -a -L 2 -I ".git|env"
