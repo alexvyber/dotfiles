ebf765e9742fe6d36b1b539e5fdbc10eaab346b7
Alex Vyber <alexvyber@gmail.com>
2022-03-30 16:28:53 +0300

    reg

--- dot/dot-config/awesome/rc.lua
+++ dot/dot-config/awesome/rc.lua
@@ -247,7 +247,7 @@ awful.screen.connect_for_each_screen(
         set_wallpaper(s)
 
         -- Each screen has its own tag table.
-        awful.tag({"  ●   ", "  ■  ",   "  ▲  ", "  ★★★  ","  ●  ", "  ■  ",  "  ▲  ",  "  ‒‒‒  ",  "  •••••  " }, s, awful.layout.layouts[1])
+        awful.tag({"  ●   ", "  ■  ",   "  ▲  ", "  ★★★  ","  ●  ", "  ■  ",  "  ▲  ",  "  ☀☀☀  ",  "  •••••  " }, s, awful.layout.layouts[1])
 
         -- Create a promptbox for each screen
         s.mypromptbox = awful.widget.prompt()
