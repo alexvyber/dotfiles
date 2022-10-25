1afab448999c1e18b41746a2885cc87816bb022e
Alexey Sokolov <alexvyber@gmail.com>
2022-10-25 11:10:13 +0300

    reg

--- dot/dot-iex.exs
+++ dot/dot-iex.exs
@@ -1,3 +1,7 @@
 IEx.configure(colors: [eval_result: [:magenta, :bright]])
 
 # import_file "~/elixir/scripts/custom_iex.exs" # script was lost in the tears like rain
+
+defmodule S do
+  def h, do: System.halt()
+end
