2793dcfaeb554ffc00bba1c951901aec478f38c2
Alexey Sokolov <alexvyber@gmail.com>
2024-08-02 17:09:29 +0300

    Aug 2, 2024, 5:09:29 PM

--- home/bin/js/bin/print-time-of-mp3-files.mjs
+++ home/bin/js/bin/print-time-of-mp3-files.mjs
@@ -4,49 +4,53 @@
  * This script calculates the duration of all files in the dirrectory from which `drra` command was called
  */
 
-import { readdir } from "node:fs"
-import { getAudioDurationInSeconds } from "get-audio-duration"
-import { Duration } from "luxon"
-import chalk from "chalk"
-const { log } = console
+import { readdir } from "node:fs";
+import { getAudioDurationInSeconds } from "get-audio-duration";
+import { Duration } from "luxon";
+import chalk from "chalk";
+const { log } = console;
 
-const fileFormats = [".mp3", ".m4b"]
+const fileFormats = [".mp3", ".m4b"];
 
-const dir = process.argv[2]
-const flag = process.argv[3]
+const dir = process.argv[2];
+const flag = process.argv[3];
 
 readdir(dir, async function (err, files) {
-  if (err) throw err
+	if (err) throw err;
 
-  const filtered = files.filter((item) => fileFormats.some((format) => item.endsWith(format)))
+	const filtered = files.filter((item) =>
+		fileFormats.some((format) => item.endsWith(format)),
+	);
 
-  if (flag === "all") {
-    const res = await Promise.all(
-      filtered.map(async (file) => ({
-        file,
-        duration: Math.floor(await getAudioDurationInSeconds(file)),
-      }))
-    )
+	if (flag === "all") {
+		const res = await Promise.all(
+			filtered.map(async (file) => ({
+				file,
+				duration: Math.floor(await getAudioDurationInSeconds(file)),
+			})),
+		);
 
-    const seconds = res.reduce((total, item) => total + item.duration, 0)
+		const seconds = res.reduce((total, item) => total + item.duration, 0);
 
-    const formatted = Duration.fromMillis(seconds * 1000).toFormat("hh:mm:ss")
+		const formatted = Duration.fromMillis(seconds * 1000).toFormat("hh:mm:ss");
 
-    log(chalk.bold(formatted))
-  }
+		log(chalk.bold(formatted));
+	}
 
-  if (!flag) {
-    const res = await Promise.all(
-      filtered.map(async (file) => ({
-        file,
-        duration: Math.floor(await getAudioDurationInSeconds(file)),
-      }))
-    )
+	if (!flag) {
+		const res = await Promise.all(
+			filtered.map(async (file) => ({
+				file,
+				duration: Math.floor(await getAudioDurationInSeconds(file)),
+			})),
+		);
 
-    res.forEach((item) =>
-      log(`${chalk.bold(Duration.fromMillis(item.duration * 1000).toFormat("hh:mm:ss"))}  -  ${item.file}`)
-    )
-  }
+		res.forEach((item) =>
+			log(
+				`${chalk.bold(Duration.fromMillis(item.duration * 1000).toFormat("hh:mm:ss"))}  -  ${item.file}`,
+			),
+		);
+	}
 
-  if (flag) log(chalk.red(`unknown flag ${flag}`))
-})
+	if (flag) log(chalk.red(`unknown flag ${flag}`));
+});
--- home/bin/js/bin/random-time.mjs
+++ home/bin/js/bin/random-time.mjs
@@ -1,25 +1,28 @@
 function minsec() {
-  return Math.floor(Math.random() * 60)
+	return Math.floor(Math.random() * 60);
 }
 
-const flag = process.argv[3]
+const flag = process.argv[3];
 
-const hours = ["9", "10", "11", "19", "20", "21"]
+const hours = ["9", "10", "11", "19", "20", "21"];
 
-const allHours = Array.from({ length: 24 }, (_, i) => i)
+const allHours = Array.from({ length: 24 }, (_, i) => i);
 
-const used = flag === "all" ? allHours : hours
+const used = flag === "all" ? allHours : hours;
 
-const index = Math.floor(used.length * Math.random())
+const index = Math.floor(used.length * Math.random());
 
-console.log(`${leftPad(used[index])}:${leftPad(minsec())}:${leftPad(minsec())}`)
+console.log(
+	`${leftPad(used[index])}:${leftPad(minsec())}:${leftPad(minsec())}`,
+);
 
 function leftPad(number) {
-  const num = Number(number)
+	const num = Number(number);
 
-  if (!(typeof num !== number) || Number.isNaN(num) || !Number.isInteger(num)) throw new Error("Must be correct number")
+	if (!(typeof num !== number) || Number.isNaN(num) || !Number.isInteger(num))
+		throw new Error("Must be correct number");
 
-  if (num < 10) return `0${num}`
+	if (num < 10) return `0${num}`;
 
-  return `${num}`
+	return `${num}`;
 }
